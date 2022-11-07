dad2edd54ca68d8bc678dc92d22e6c2c8042d7d3
TShokhrukh <shohruh8945@gmail.com>
2022-11-07 22:34:13 +0500

    update map constants

--- src/constants/map.ts
+++ src/constants/map.ts
@@ -1,6 +1,8 @@
 
-export const MAP_WIDTH = 640
-export const MAP_HEIGHT = 670
+export const MAP_CELL_WIDTH = 18
+export const MAP_CELL_HEIGHT = 18
 
-export const MAP_CELL_WIDTH = 10
-export const MAP_CELL_HEIGHT = 10
+export const MAP_WIDTH = MAP_CELL_WIDTH * 15
+export const MAP_HEIGHT = MAP_CELL_HEIGHT * 20
+
+export const MAP_CELL_COLORS = ['#66bb6a', '#a5d6a7']

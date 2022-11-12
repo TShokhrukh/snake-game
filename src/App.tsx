15e1de4a990d2a38835a185b57e1bdc97d0584a9
TShokhrukh <shohruh8945@gmail.com>
2022-11-12 21:56:28 +0500

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Game } from './components/Game/Game'
+import { store } from './store'
+import './css/main.css'
 
 function App () {
   return (
-    <div>
-
-    </div>
+    <React.Fragment>
+      <Game map={store.map} />
+    </React.Fragment>
   )
 }
 

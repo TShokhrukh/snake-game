2950e98e55e4baf7c6b7aa58a88929fafa1c1173
TShokhrukh <shohruh8945@gmail.com>
2022-12-06 23:26:51 +0500

    update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,50 @@
-export * from './store-types'
-export * from './ctx-types'
+export type TMoveAction = 'top'|'right'|'bottom'|'left'
+export type TGameStatus = 'playing'|undefined
+
+export interface ICell {
+  x: number
+  y: number
+}
+
+export interface IMap {
+  cells: ICell[]
+
+  rows: number
+  columns: number
+  grid: (ICell[])[]
+}
+
+export interface ISnake {
+  position: ICell[]
+
+  get head(): ICell
+  get body(): ICell[]
+  get tail(): ICell
+  get isDed(): boolean
+  get length(): number
+
+  grow(): void
+  die(): void
+  move(action: TMoveAction): void
+  canMoveTo(action: TMoveAction): boolean
+}
+
+export interface IFood {
+  position: ICell
+
+  setPosition (position: ICell): void
+}
+
+export interface ICtx {
+  draw (ctx: CanvasRenderingContext2D): void
+}
+
+export interface IGame {
+  status: TGameStatus
+
+  get isPlaying(): boolean
+
+  start(ctx: CanvasRenderingContext2D): void
+  restart(): void
+  stop(): void
+}
